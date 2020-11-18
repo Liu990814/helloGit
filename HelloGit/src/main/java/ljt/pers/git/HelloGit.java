d475241e6e04dce83b7d9b3de67df5c99112d941
ljt_glo <ljt_glo@qq.com>
2020-11-18 18:35:05 +0800

    idea第一次添加测试

---
+++ HelloGit/src/main/java/ljt/pers/git/HelloGit.java
@@ -0,0 +1,11 @@
+package ljt.pers.git;
+
+/**
+ * @author ljt
+ * @create 2020-11-18 18:18
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
